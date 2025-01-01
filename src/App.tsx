0ff1d1936b0d4f6024fd4f09ad432342f9aa48e7
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-01 18:40:57 +0800

    tambah

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import React from 'react'
 import Dice from './Components/Dice' 
 
-function allNewDice() {
-  const newDice = []
-  for ( let i = 0; i < 10; i++){
-    newDice.push(Math.ceil(Math.random() * 6))
-  }
-  return newDice
-}
-
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+
+  function allNewDice() {
+    const newDice = []
+    for ( let i = 0; i < 12; i++){
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+    //ini ada key id
+  const diceElement = dice.map((value, index) => (
+     <Dice key={index} value={value} />
+  ))
+
+  //ini tanpa key id
+  const diceEle = dice.map( die => 
+    <Dice value={die} />
+   )
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-8">Tenzies</h1>
       <div className="grid grid-cols-4 gap-4 mb-8">
-        <Dice value='1' />
-        <Dice value='2' />
-        <Dice value='3' />
-        <Dice value='4' />
-        <Dice value='5' />
-        <Dice value='6' />
-        <Dice value='1' />
-        <Dice value='1' />
-        <Dice value='1' />
-        <Dice value='1' />
-        <Dice value='1' />
-        <Dice value='1' />
+        {diceElement}
+        {diceEle}
       </div>
       <button className="px-4 py-2 bg-blue-500 text-white rounded-md">Roll</button>
     </main>
