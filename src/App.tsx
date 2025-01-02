e1a9d24c63e2cfc5b81f7758dd95e62a8aa2fa7c
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-02 12:07:51 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,31 @@ import Dice from './Components/Dice'
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
-  function reset() {
-    setDice(allNewDice())
+
+
+  function diceGenerate(){
+    return {
+      value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
   }
 
   function allNewDice() {
     const newDice = []
     for ( let i = 0; i < 12; i++){
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(diceGenerate())
     }
     return newDice
   }
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? 
+            die :
+            diceGenerate()
+    }))
+}
 
   function holdDIce(id: string) {
     setDice (prevDice => prevDice.map( dice => 
@@ -43,7 +53,7 @@ function App() {
       <div className="grid grid-cols-4 gap-4 mb-8">
         {diceElement}
       </div>
-      <button onClick={reset}
+      <button onClick={rollDice}
       className="px-4 py-2 bg-blue-500 text-white rounded-md">Roll</button>
     </main>
   )
